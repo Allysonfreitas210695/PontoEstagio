2711d79d3b5598b7ed7b2b07f3cf198ad648ed2b
Allysonfreitas210695 <alisonfr83@gmail.com>
2025-04-22 19:34:07 -0300

    test(domain): adicionando teste do domain da entidade de frequência

--- Backend/tests/CommonTestUltilities/Entities/CompanyBuilder.cs
+++ Backend/tests/CommonTestUltilities/Entities/CompanyBuilder.cs
@@ -6,22 +6,21 @@ namespace CommonTestUltilities.Entities;
 
 public class CompanyBuilder
 { 
-        public static Company Build(
-            Guid? id = null,
-            string? name = null, 
-            string? cnpj = null,  
-            string? phone = null, 
-            string? email = null
-           )
-        {
-            var faker = new Faker();
-
-            return new Company(
-                id: id ?? Guid.NewGuid(),
-                name: name ?? faker.Company.CompanyName(),
-                cnpj: cnpj ?? faker.Company.Cnpj(),
-                phone: phone ?? faker.Phone.PhoneNumber(),
-                email: email ?? faker.Internet.Email()
-            );
-        } 
+    public static Company Build(
+        Guid? id = null,
+        string? name = null, 
+        string? cnpj = null,  
+        string? phone = null, 
+        string? email = null
+       )
+    {
+        var faker = new Faker();
+        return new Company(
+            id: id ?? Guid.NewGuid(),
+            name: name ?? faker.Company.CompanyName(),
+            cnpj: cnpj ?? faker.Company.Cnpj(),
+            phone: phone ?? faker.Phone.PhoneNumber(),
+            email: email ?? faker.Internet.Email()
+        );
+    } 
 }
