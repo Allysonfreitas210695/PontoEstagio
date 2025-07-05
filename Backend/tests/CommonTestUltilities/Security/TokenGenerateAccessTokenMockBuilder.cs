3370877e418506d94321a6c89de8aa96c6aa05f8
Allysonfreitas210695 <alisonfr83@gmail.com>
2025-07-05 20:43:06 -0300

    feat: Adicionando mock de seguraça

---
+++ Backend/tests/CommonTestUltilities/Security/TokenGenerateAccessTokenMockBuilder.cs
@@ -0,0 +1,28 @@
+﻿using Moq;
+using PontoEstagio.Domain.Entities;
+using PontoEstagio.Domain.Security.Token;
+
+namespace CommonTestUltilities.Security
+{
+    public class TokenGenerateAccessTokenMockBuilder
+    {
+        private readonly Mock<ITokenGenerateAccessToken> _mock;
+
+        public TokenGenerateAccessTokenMockBuilder()
+        {
+            _mock = new Mock<ITokenGenerateAccessToken>();
+        }
+
+        public TokenGenerateAccessTokenMockBuilder SetupGenerateAccessToken(User user, string token)
+        {
+            _mock.Setup(x => x.GenerateAccessToken(user))
+                .Returns(token);
+            return this;
+        }
+
+        public ITokenGenerateAccessToken Build()
+        {
+            return _mock.Object;
+        }
+    }
+}
