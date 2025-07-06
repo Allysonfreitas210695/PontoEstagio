6c3e69abe4e4e715ed1d761f682461b612c2460a
Allysonfreitas210695 <alisonfr83@gmail.com>
2025-07-05 21:26:58 -0300

    chore: Ajuste nas tabelas

--- Backend/src/PontoEstagio.Infrastructure/Migrations/20250706002247_UpdateColunmTableUsers.Designer.cs
+++ Backend/src/PontoEstagio.Infrastructure/Migrations/20250706002247_UpdateColunmTableUsers.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using PontoEstagio.Infrastructure.Context;
@@ -11,9 +12,11 @@ using PontoEstagio.Infrastructure.Context;
 namespace PontoEstagio.Infrastructure.Migrations
 {
     [DbContext(typeof(PontoEstagioDbContext))]
-    partial class PontoEstagioDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250706002247_UpdateColunmTableUsers")]
+    partial class UpdateColunmTableUsers
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
