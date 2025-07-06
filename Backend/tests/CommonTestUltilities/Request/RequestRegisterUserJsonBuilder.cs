031e102229884a4649faea090dbf8527656e5a88
Allysonfreitas210695 <alisonfr83@gmail.com>
2025-07-05 21:27:24 -0300

    fix: Ajuste no teste de validators

--- Backend/tests/CommonTestUltilities/Request/RequestRegisterUserJsonBuilder.cs
+++ Backend/tests/CommonTestUltilities/Request/RequestRegisterUserJsonBuilder.cs
@@ -2,33 +2,72 @@
 using PontoEstagio.Communication.Enum;
 using PontoEstagio.Communication.Request;
 using System;
-using System.Net.NetworkInformation;
 
-namespace CommonTestUltilities.Request
+namespace CommonTestUltilities.Request;
+
+public class RequestRegisterUserJsonBuilder
 {
-    public class RequestRegisterUserJsonBuilder
+    public static RequestRegisterUserJson Build(UserType? userType = null)
     {
-        public static RequestRegisterUserJson Build()
-        {
-            var faker = new Faker();
+        var faker = new Faker("pt_BR");
+        var type = userType ?? faker.PickRandom<UserType>();
 
-            return new Faker<RequestRegisterUserJson>()
-                .RuleFor(x => x.Name, f => f.Name.FullName())
-                .RuleFor(x => x.Email, f => f.Internet.Email())
-                .RuleFor(x => x.Registration, f => f.Random.AlphaNumeric(10).ToUpper())
-                .RuleFor(x => x.Password, f => f.Internet.Password(prefix: "!Aa"))
-                .RuleFor(x => x.UniversityId, f => Guid.NewGuid())
-                .RuleFor(x => x.CourseId, (f, x) =>
-                {
-                    if (x.Type == UserType.Intern || x.Type == UserType.Coordinator)
-                        return Guid.NewGuid();
-                    else
-                        return null;
-                })
-                .RuleFor(x => x.isActive, f => f.Random.Bool())
-                .RuleFor(x => x.Type, f => f.Random.Enum<UserType>())
-                .RuleFor(x => x.Phone, f => f.Phone.PhoneNumber())
-                .Generate();
+        var request = new RequestRegisterUserJson
+        {
+            Name = faker.Name.FullName(),
+            Email = faker.Internet.Email(),
+            Password = "ValidPass123!", // Senha que atende ao PasswordValidator
+            UniversityId = Guid.NewGuid(),
+            isActive = true,
+            Type = type
+        };
+
+        // Configurações específicas por tipo
+        switch (type)
+        {
+            case UserType.Intern:
+                request.CourseId = Guid.NewGuid();
+                request.Registration = faker.Random.AlphaNumeric(10).ToUpper();
+                request.Phone = faker.Phone.PhoneNumber("###########");
+                break;
+
+            case UserType.Coordinator:
+                request.CourseId = Guid.NewGuid();
+                request.Registration = faker.Random.AlphaNumeric(10).ToUpper();
+                request.Phone = faker.Phone.PhoneNumber("###########");
+                break;
+
+            case UserType.Advisor:
+                request.CPF = faker.Random.ReplaceNumbers("###########");
+                request.Department = faker.Commerce.Department();
+                break;
+
+            case UserType.Supervisor:
+                // Nenhum campo adicional obrigatório
+                break;
         }
+
+        return request;
+    }
+
+    // Métodos adicionais para cenários específicos
+    public static RequestRegisterUserJson BuildIntern()
+    {
+        return Build(UserType.Intern);
+    }
+
+    public static RequestRegisterUserJson BuildCoordinator()
+    {
+        return Build(UserType.Coordinator);
+    }
+
+    public static RequestRegisterUserJson BuildAdvisor()
+    {
+        return Build(UserType.Advisor);
+    }
+
+    public static RequestRegisterUserJson BuildSupervisor()
+    {
+        return Build(UserType.Supervisor);
     }
 }
